46f678d59966d2cb1ffffea9f060846b62c3cb70
Alexander Hodges <ahodges@localhost.localdomain>
2018-02-11 13:08:54 +0100

    Added a triangle to the render loop.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,20 @@ std::vector<float> processColor(std::vector<float> colorVector);
 int SCR_WIDTH = 0;
 int SCR_HEIGHT = 0;
 
+const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+
+
 
 int main() {
     //Setup glfw profile
@@ -38,18 +52,79 @@ int main() {
         return -1;
     }
     glfwMakeContextCurrent(window);
-
-
-    //Initialises GLAD (using GLEW instead)
-    /*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }*/
+    glewInit();
 
     //Sets OPENGl viewport and registers callback function to resize it.
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+// build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    // fragment shader
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+
+
+
+
+
+    float vertices[] = {
+            -0.5f,-0.5f,0.0f,
+            0.5f,-0.5f,0.0f,
+            0.0f,0.5f,0.0f
+    };
+
+
+
+
+
+    //Generate Buffer
+    unsigned int VBO,VAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1,&VAO);
+
+    glBindVertexArray(VAO);
+    //Bind buffer to Array Buffer
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+
+    //Copy vertex data into buffer memory
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
 
     std::vector<float> colorVector = {0.0f,0.0f,0.0f,1.0f};
     //Render Loop
@@ -59,9 +134,16 @@ int main() {
         processInput(window);
 
         //rendering commands
-        colorVector = processColor(colorVector);
-        glClearColor(colorVector[0],colorVector[1],colorVector[2],colorVector[3]);
+
+        //colorVector = processColor(colorVector);
+        //glClearColor(colorVector[0],colorVector[1],colorVector[2],colorVector[3]);
+        glClearColor(0.0f,0.0f,0.0f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,3);
+
 
         //check and call events and swap buffers
         glfwSwapBuffers(window);
@@ -100,3 +182,14 @@ std::vector<float> processColor(std::vector<float> colorVector)
     colorVector[2] = b;
     return colorVector;
 }
+
+
+
+
+
+
+
+
+
+
+
